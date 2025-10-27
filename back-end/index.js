52d367def6b170d3ed762d6e2b7bb8fd28b0a6fc
pedrohkf <phkfaria@gmail.com>
2025-10-27 10:10:20 -0300

    feat(back): arrumar importação no index.js

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 const authRoutes = require('./src/routes/authRoutes');
+const pizzariaRoutes = require('./src/routes/pizzariaRoutes');
 
 require('dotenv').config();
 const app = express();
